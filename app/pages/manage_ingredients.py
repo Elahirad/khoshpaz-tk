e4e925fcd57d1284df5a80136e8e5e0466bc49c3
Ali Elahirad <83788370+Elahirad@users.noreply.github.com>
2024-06-28 16:34:45 +0330

    Used context for ingredients.

--- app/pages/manage_ingredients.py
+++ app/pages/manage_ingredients.py
@@ -2,18 +2,16 @@ from customtkinter import CTkFrame, CTkLabel, CTkButton
 
 from app.constants import normal_text_font
 from app.pages.components import Table, InputFrame
+from app.context import Context
 
 
 class ManageIngredients(CTkFrame):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
-        fake_data = [
-            {"نام": "شکر", "مقدار": "2 کیلو", "تاریخ ورود": "1402/03/01", "قیمت": "20000 تومان"},
-            {"نام": "روغن", "مقدار": "5 لیتر", "تاریخ ورود": "1402/03/05", "قیمت": "150000 تومان"},
-            {"نام": "آرد", "مقدار": "10 کیلو", "تاریخ ورود": "1402/03/10", "قیمت": "70000 تومان"},
-        ]
-        self.data = fake_data
+        self.__context = Context()
+        self.__context.add_callback(self.__update_table)
+        self.data = self.__context['ingredients']
 
         CTkLabel(
             master=self, text="مدیریت مواد اولیه", font=("B Koodak Bold", 25)
@@ -41,6 +39,15 @@ class ManageIngredients(CTkFrame):
         self.__report_button1.pack(side="right", padx=5)
         self.__report_button2.pack(side="right", padx=5)
         self.__report_button3.pack(side="right", padx=5)
+
+    def __update_table(self):
+        self.data = self.__context['ingredients']
+        self.__table.pack_forget()
+        self.__report_frame.pack_forget()
+        self.__table = Table(self.data, self.__columns, master=self, width=self.winfo_width(),
+                             height=self.winfo_height())
+        self.__table.pack(pady=10, padx=10, fill="both", expand=True)
+        self.__report_frame.pack(pady=10, fill="x")
 
     def __report1(self): print("۱ گزارش اجرا شد")
 
