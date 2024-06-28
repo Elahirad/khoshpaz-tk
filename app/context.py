03b11ea1cb04ae8582f025f15afb7ca0cb874897
Ali Elahirad <83788370+Elahirad@users.noreply.github.com>
2024-06-28 13:11:07 +0330

    Implemented Context and refactored the codes.

---
+++ app/context.py
@@ -0,0 +1,26 @@
+from typing import Callable
+from .tools import singleton
+
+
+@singleton
+class Context:
+    def __init__(self):
+        self.__data: dict = {}
+        self.__callbacks: list[Callable] = []
+
+    def add_callback(self, callback: Callable) -> None:
+        self.__callbacks.append(callback)
+
+    def remove_callback(self, callback: Callable) -> None:
+        self.__callbacks.remove(callback)
+
+    def __setitem__(self, key, value):
+        self.__data[key] = value
+        self.__notify_callbacks()
+
+    def __getitem__(self, key):
+        return self.__data[key]
+
+    def __notify_callbacks(self):
+        for callback in self.__callbacks:
+            callback()
