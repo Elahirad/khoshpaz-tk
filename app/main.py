343bbf748d32b92e82e8ac0285ef81e29a1a5d55
NYXTheShadow <alielahirad8087@gmail.com>
2024-06-28 16:34:45 +0330

    Used context for ingredients.

--- app/main.py
+++ app/main.py
@@ -13,9 +13,16 @@ from .context import Context
 class App(CTk):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.__main_view = None
+        self.__sidebar = None
         self.__context = Context()
         self.__context['mode'] = MANAGE_ORDERS
-
+        fake_gredients = [
+            {"نام": "شکر", "مقدار": "2 کیلو", "تاریخ ورود": "1402/03/01", "قیمت": "20000 تومان"},
+            {"نام": "روغن", "مقدار": "5 لیتر", "تاریخ ورود": "1402/03/05", "قیمت": "150000 تومان"},
+            {"نام": "آرد", "مقدار": "10 کیلو", "تاریخ ورود": "1402/03/10", "قیمت": "70000 تومان"},
+        ]
+        self.__context['ingredients'] = fake_gredients
         # Geometry
         self.geometry("850x650")
         self.resizable(False, False)
@@ -25,6 +32,12 @@ class App(CTk):
         set_default_color_theme("green")
         self.after(200, lambda: self.iconbitmap("./app/assets/logo.ico"))
         self.title("کترینگ خوش‌پز")
+
+        self.__build_view()
+
+    def __build_view(self):
+        for child in self.winfo_children():
+            child.destroy()
 
         # Sidebar view
         self.__sidebar = SideBar(self, width=170, height=650)
