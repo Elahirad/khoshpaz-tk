1b12399fd7de00e83f838e930996fa56661ba232
NYXTheShadow <alielahirad8087@gmail.com>
2024-06-30 19:36:49 +0330

    Major refactoring for Customer, Food and Ingredient management.

---
+++ app/view/pages/components/form_interface.py
@@ -0,0 +1,27 @@
+from typing import Callable
+from abc import ABC, abstractmethod
+
+from customtkinter import CTkLabel, CTkToplevel
+
+from app.view.context import Context
+
+
+class IForm(CTkToplevel, ABC):
+    def __init__(self, title: str, default_values: dict[str, str], callback: Callable, *args,
+                 **kwargs) -> None:
+        super().__init__(master=None, *args, **kwargs)
+        self.geometry("400x700")
+        self.resizable(False, False)
+
+        self._context = Context()
+
+        self._current_values = default_values
+
+        self._callback = callback
+        CTkLabel(
+            master=self, text=title, font=("B Koodak Bold", 25)
+        ).pack(fill="x", anchor="center", pady=10)
+
+    @abstractmethod
+    def _submit(self) -> None:
+        pass
--- app/view/pages/manage_ingredients/main.py
+++ app/view/pages/manage_ingredients/main.py
@@ -1,94 +1,41 @@
-from customtkinter import CTkFrame, CTkLabel, CTkButton
+from customtkinter import CTkButton
 
 from app.view.constants import normal_text_font
-from app.view.pages.components import Table
-from app.view.context import Context
-from .ingredient_delete import IngredientDelete
-from .ingredient_update_form import IngredientUpdateForm
-from .ingredient_input_form import IngredientInputForm
+from app.view.pages.components import IDataView
+from .ingredient_form import IngredientForm
 
 
-class ManageIngredients(CTkFrame):
+class ManageIngredients(IDataView):
     def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+        super().__init__('ingredients', 'ماده‌ی اولیه', 'مواد اولیه', [('name', 'نام'), ('unit', 'واحد')],
+                         {'id': 0, 'name': '', 'unit': ''}, IngredientForm, *args,
+                         **kwargs)
 
-        self.__context = Context()
-        self.__context.add_callback(self.__update_table)
-        self.data = self.__context['ingredients']
+        self._report_button1 = CTkButton(master=self._report_frame, text="۱ گزارش", command=self._report1,
+                                         font=normal_text_font)
+        self._report_button2 = CTkButton(master=self._report_frame, text="۲ گزارش", command=self._report2,
+                                         font=normal_text_font)
+        self._report_button3 = CTkButton(master=self._report_frame, text="۳ گزارش", command=self._report3,
+                                         font=normal_text_font)
+        self._report_button1.pack(side="right", padx=5)
+        self._report_button2.pack(side="right", padx=5)
+        self._report_button3.pack(side="right", padx=5)
 
-        CTkLabel(
-            master=self, text="مدیریت مواد اولیه", font=("B Koodak Bold", 25)
-        ).pack(fill="x", anchor="center", pady=10)
+    def _add_callback(self, data):
+        self._context.controller.add_ingredient(data['name'], data['unit'])
 
-        self.__columns = [('name', 'نام'), ('unit', 'واحد')]
+    def _delete_callback(self, confirmed: bool, item_id: int):
+        if confirmed:
+            self._context.controller.remove_ingredient(item_id)
 
-        def show_input_modal():
-            self.__input_window = IngredientInputForm(self.__add_callback)
-            self.__input_window.grab_set()
+    def _update_callback(self, entity_id: int, item: dict):
+        self._context.controller.update_ingredient(entity_id, item['name'], item['unit'])
 
-        self.__input_window = None
-
-        self.__update_window = None
-        self.__delete_window = None
-
-        self.__add_button_frame = CTkFrame(self, fg_color='transparent')
-        self.__add_button_frame.pack(pady=10, padx=10, fill="x")
-        self.__add_button = CTkButton(self.__add_button_frame, text='افزودن', font=('B Koodak Bold', 25),
-                                      command=show_input_modal)
-        self.__add_button.pack()
-
-        self.__table = Table(self.data, self.__columns, master=self, width=self.winfo_width(),
-                             height=self.winfo_height(), delete_callback=self.__delete_callback,
-                             update_callback=self.__update_callback)
-        self.__table.pack(pady=10, padx=10, fill="both", expand=True)
-
-        # Report buttons
-        self.__report_frame = CTkFrame(master=self, fg_color='transparent')
-        self.__report_frame.pack(pady=10, fill="x")
-
-        self.__report_button1 = CTkButton(master=self.__report_frame, text="۱ گزارش", command=self.__report1,
-                                          font=normal_text_font)
-        self.__report_button2 = CTkButton(master=self.__report_frame, text="۲ گزارش", command=self.__report2,
-                                          font=normal_text_font)
-        self.__report_button3 = CTkButton(master=self.__report_frame, text="۳ گزارش", command=self.__report3,
-                                          font=normal_text_font)
-        self.__report_button1.pack(side="right", padx=5)
-        self.__report_button2.pack(side="right", padx=5)
-        self.__report_button3.pack(side="right", padx=5)
-
-    def __update_table(self):
-        self.data = self.__context['ingredients']
-        self.__table.pack_forget()
-        self.__report_frame.pack_forget()
-        self.__table = Table(self.data, self.__columns, master=self, width=self.winfo_width(),
-                             height=self.winfo_height(), delete_callback=self.__delete_callback,
-                             update_callback=self.__update_callback)
-        self.__table.pack(pady=10, padx=10, fill="both", expand=True)
-        self.__report_frame.pack(pady=10, fill="x")
-
-    def __add_callback(self, name, unit):
-        self.__context.controller.add_ingredient(name, unit)
-
-    def __delete_callback(self, item_id: int):
-        def delete(confirmed: bool):
-            if confirmed:
-                self.__context.controller.remove_ingredient(item_id)
-
-        self.__delete_window = IngredientDelete(delete)
-        self.__delete_window.grab_set()
-
-    def __update_callback(self, item: dict):
-        def update(data):
-            self.__context.controller.update_ingredient(item['id'], data['name'], data['unit'])
-
-        self.__update_window = IngredientUpdateForm(item, update)
-        self.__update_window.grab_set()
-
-    def __report1(self):
+    def _report1(self):
         print("۱ گزارش اجرا شد")
 
-    def __report2(self):
+    def _report2(self):
         print("۲ گزارش اجرا شد")
 
-    def __report3(self):
+    def _report3(self):
         print("۳ گزارش اجرا شد")
